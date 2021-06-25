969454413070e98b41e2ba55ec5988919341ba7c
mnaovi <nazmulahsan210@gmail.com>
2021-06-26 03:24:56 +0600

    init commit

---
+++ expiration/src/events/listeners/order-created-listerner.ts
@@ -0,0 +1,25 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@naotickets/common';
+import { Message } from 'node-nats-streaming'; 
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class orderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log('Waiting for some time to process the job:', delay);
+
+        await expirationQueue.add(
+            {
+                orderId: data.id,
+            },
+            {
+                delay
+            }
+        );
+
+        msg.ack();
+    }
+}
---
+++ orders/src/routes/show.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@naotickets/common';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+});
+    
+export { router as showOrderRouter };              
